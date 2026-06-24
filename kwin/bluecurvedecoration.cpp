db5b2e668356daf2b56b2406d0c5bc104d8016fe
neeeeow <166566700+neeeeow@users.noreply.github.com>
2026-06-24 22:48:21 +0100

    Add copyright notice

--- kwin/bluecurvedecoration.cpp
+++ kwin/bluecurvedecoration.cpp
@@ -1,3 +1,30 @@
+/*
+  Bluecurve KDecoration 3 theme.
+
+  Copyright (c) 2026 neeeeow
+  Author: neeeeow (https://github.com/neeeeow/Bluecurve-Qt)
+
+  Painting logic based on the Bluecurve KDE 3 theme:
+  Copyright (c) 1999, 2001 Red Hat, Inc.
+  Authors: Daniel Duley <mosfet@kde.org>,
+           Matthias Ettrich <ettrich@kde.org>
+		   Karol Szwed <gallium@kde.org>
+		   Than Ngo <than@redhat.com>
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/ 
+
 #include "bluecurvedecoration.h"
 #include "bitmaps.h"
 
